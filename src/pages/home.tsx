f93b8ead544b1e74d206d8bcceeee44ab5d5c43a
laomeowmeow <vincent.niew@iCloud.com>
2025-02-28 19:51:46 +0800

    Add the codes

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,11 +1,23 @@
 // pages/home.tsx
-import { Container, Typography } from "@mui/material";
+import { Button, Container, Typography } from "@mui/material";
+import Router, { useRouter } from "next/router";
 
 export default function HomePage() {
+  const router = useRouter();
+
+  const navigateIndex = () => {
+    Router.push("/index");
+  };
+
   return (
     <Container>
       <Typography variant="h4">Welcome to the Home Page!</Typography>
-      
+      <Button
+        variant="contained"
+        color="primary"
+        onClick={navigateIndex}
+        style={{ margin: "10px" }}
+      >Index</Button>
     </Container>
   );
 }
