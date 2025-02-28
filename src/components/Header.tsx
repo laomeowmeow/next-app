cbc604f145d22f3c4943bafb1e987af1428a4419
laomeowmeow <vincent.niew@iCloud.com>
2025-02-28 16:05:30 +0800

    Partial - Testing with the Header component

---
+++ src/components/Header.tsx
@@ -0,0 +1,8 @@
+import { Typography } from "@mui/material";
+import Link from "next/Link";
+
+const Header: React.FC = () => {
+    return (
+        <Typography variant="h4">Header</Typography>
+    );
+};
